9310d8ac9d3d0ac1e1179d6b116bb63dc0901892
StudioCatProductions <gnomatix.music@gmail.com>
2021-03-18 12:03:36 -0400

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,19 +1,20 @@
-document.addEventListener('DOMContentLoaded', function()){
-window.onscroll = function(){headerControl()}
+document.addEventListener('DOMContentLoaded', function(){
+window.onscroll = function(){headerControl();};
 var header=document.getElementById("header");
 var blah=document.getElementById("blah");
-var opaque = blah.offsetTop
+var opaque = blah.offsetTop;
 function headerControl(){
    if (window.pageYOffset > opaque){
      header.classList.add("opaque");
 }
-  else{header.classList.remove("opaque")
+  else{header.classList.remove("opaque");
 }
 }
 function sendMail(){
-  var link = "mailto:gnomatix.music@gmail.com"
-+"?cc="+ encodeURIComponent(document.getElementById("email").value)
-+"&subject=" + encodeURIComponent(document.getElementById("subject").value)
-+"&body=" + encodeURIComponent(document.getElementById("body").value)
-  window.location.href = link}
-}
+  var link = "mailto:gnomatix.music@gmail.com"+"?cc="
+  + encodeURIComponent(document.getElementById("email").value)+"&subject="
+  + encodeURIComponent(document.getElementById("subject").value)+"&body="
+  + encodeURIComponent(document.getElementById("body").value);
+
+  window.location.href = link;}
+});
