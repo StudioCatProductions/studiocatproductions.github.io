448e22c29916dd1739d04ae9a16a509339ee9de2
StudioCatProductions <gnomatix.music@gmail.com>
2021-04-01 18:43:56 -0400

    form blah

--- myscripts.js
+++ myscripts.js
@@ -10,11 +10,4 @@ function headerControl(){
   else{header.classList.remove("opaque");
 }
 }
-function sendMail(){
-  var link = "mailto:gnomatix.music@gmail.com"+"?cc="
-  + encodeURIComponent(document.getElementById("email").value)+"&subject="
-  + encodeURIComponent(document.getElementById("subject").value)+"&body="
-  + encodeURIComponent(document.getElementById("body").value);
-
-  window.location.href = link;}
 });
